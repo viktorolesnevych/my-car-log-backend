15c6c5d46cfaf6ae94963fc3f868395e07640b5d
Viktor Olesnevych <viktor.olesnevych@hotmail.com>
2021-05-13 12:34:47 -0500

    Initial commit

---
+++ src/main/java/com/mycarlog/mycarlog/MyCarLogApplication.java
@@ -0,0 +1,13 @@
+package com.mycarlog.mycarlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycarlog/mycarlog/MyCarLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycarlog.mycarlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCarLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
